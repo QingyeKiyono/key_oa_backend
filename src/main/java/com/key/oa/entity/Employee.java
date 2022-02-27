60f39616f502f8b4d5c9712ebd54cf5aa58d6e98
jyjdal <1604070358@qq.com>
2022-02-27 14:28:22 +0800

    修改主键生成策略，删除hibernate_sequence表

--- src/main/java/com/key/oa/entity/Employee.java
+++ src/main/java/com/key/oa/entity/Employee.java
@@ -21,7 +21,7 @@ public class Employee {
      * 主键Id
      */
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     /**
