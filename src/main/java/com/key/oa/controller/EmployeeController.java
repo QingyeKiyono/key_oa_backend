dc10173c3c2e1f00a73ee8f31ff8f8b9b71beb91
九月家的敖烈 <1604070358@qq.com>
2022-05-05 22:24:54 +0800

    feature(Controller): 为EmployeeController添加新的功能
    1. 添加根据员工工号查看员工信息的功能

--- src/main/java/com/key/oa/controller/EmployeeController.java
+++ src/main/java/com/key/oa/controller/EmployeeController.java
@@ -3,14 +3,12 @@ package com.key.oa.controller;
 import com.key.oa.common.JsonResponse;
 import com.key.oa.entity.Employee;
 import com.key.oa.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +17,7 @@ import java.util.List;
  *
  * @author 孙强
  */
+@Slf4j
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
@@ -34,11 +33,17 @@ public class EmployeeController {
     public JsonResponse<List<Employee>> getEmployeeList(@RequestParam int page, @RequestParam int size) {
         int sizeMax = 20;
 
-        if (size > sizeMax) {
+        if (size > sizeMax || page < 1) {
             throw new IllegalArgumentException();
         }
 
-        Page<Employee> employees = this.employeeService.findAll(PageRequest.of(page, size));
+        Page<Employee> employees = this.employeeService.findAll(PageRequest.of(page - 1, size));
         return new JsonResponse<>(employees.stream().toList());
+    }
+
+    @GetMapping("/{jobNumber}")
+    @PreAuthorize("hasAuthority('dev:test')")
+    public JsonResponse<Employee> getEmployeeByJobNumber(@PathVariable String jobNumber) {
+        return new JsonResponse<>(this.employeeService.findByJobNumber(jobNumber));
     }
 }
