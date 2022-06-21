0f7d8e793a433ba3a89548e7a66ca049cdec4981
九月家的敖烈 <1604070358@qq.com>
2022-06-21 22:27:46 +0800

    feature(Security): 升级Spring Security
    1. 升级Spring Security版本到5.7.0
    2. 升级配置方式，使用名称更易于理解的类名
    
    BREAKING CHANGE: Spring Security的WebSecurityConfigurerAdapter被废弃，对应的API也被废弃

--- src/main/java/com/key/oa/config/SpringSecurityConfig.java
+++ src/main/java/com/key/oa/config/SpringSecurityConfig.java
@@ -6,38 +6,41 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
- * @author 孙强
  * 用于配置Spring Security
+ *
+ * @author 孙强
  */
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
+@EnableWebSecurity
+public class SpringSecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     private final UserDetailsService userDetailsService;
 
     @Autowired
-    public WebSecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, UserDetailsService userDetailsService) {
+    public SpringSecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, UserDetailsService userDetailsService) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
         this.userDetailsService = userDetailsService;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 // 取消csrf限制，之后可能会引起安全问题
                 .csrf().disable()
@@ -53,6 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // 覆盖掉原有的logout接口，否则会自动重定向而无法正常完成功能
         http.logout().logoutUrl("/defaultLogout");
         http.antMatcher("/defaultLogout");
+        return http.build();
     }
 
     @Bean
@@ -60,11 +64,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    /**
-     * 用于解决Spring Security在抛出UsernameNotFoundException时无法捕获到的错误
-     *
-     * @return 修改后的DaoAuthenticationProvider
-     */
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
