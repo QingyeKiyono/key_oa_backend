be21473e189a777ccf7c59f4f93c852eee599b88
九月家的敖烈 <1604070358@qq.com>
2022-07-17 15:32:40 +0800

    refactor(Page): 代码维护
    1. 在PageRepository上添加@Repository注解
    2. 删除未使用的import

--- src/main/java/com/key/oa/repository/PageRepository.java
+++ src/main/java/com/key/oa/repository/PageRepository.java
@@ -3,9 +3,11 @@ package com.key.oa.repository;
 import com.key.oa.entity.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author 孙强
  */
+@Repository
 public interface PageRepository extends JpaRepository<Page, Long>, JpaSpecificationExecutor<Page> {
 }
--- src/test/java/com/key/oa/event/PageTest.java
+++ src/test/java/com/key/oa/event/PageTest.java
@@ -1,6 +1,5 @@
 package com.key.oa.event;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.key.oa.common.JsonResponse;
