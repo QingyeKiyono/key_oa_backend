90b09e621eeb93acb75ed51727cd482be1b43dbc
jyjdal <1604070358@qq.com>
2023-01-27 20:42:06 +0800

    Reimplement repositories in Kotlin.

---
+++ src/main/java/com/key/oa/repository/PageResRepository.kt
@@ -0,0 +1,10 @@
+package com.key.oa.repository
+
+import com.key.oa.entity.PageRes
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PageResRepository : JpaRepository<PageRes, Long>, JpaSpecificationExecutor<PageRes> {
+}
