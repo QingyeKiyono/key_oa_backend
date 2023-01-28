d37d560c22632bddbc838ed7d1983f3acd7bac11
jyjdal <1604070358@qq.com>
2023-01-28 20:19:32 +0800

    Reimplement JsonResponse in Kotlin.

--- src/main/java/com/key/oa/common/JsonResponse.kt
+++ src/main/java/com/key/oa/common/JsonResponse.kt
@@ -3,22 +3,12 @@ package com.key.oa.common
 class JsonResponse<T> private constructor(val code: String, val message: String, val data: T?) {
     companion object {
         @JvmStatic
-        fun <T> success(): JsonResponse<T> {
-            return JsonResponse("00000", "OK", null)
-        }
-
-        @JvmStatic
-        fun <T> success(data: T): JsonResponse<T> {
+        fun <T> success(data: T? = null): JsonResponse<T> {
             return JsonResponse("00000", "OK", data)
         }
 
         @JvmStatic
-        fun <T> error(code: String, message: String): JsonResponse<T> {
-            return JsonResponse(code, message, null)
-        }
-
-        @JvmStatic
-        fun <T> error(code: String, message: String, data: T): JsonResponse<T> {
+        fun <T> error(code: String, message: String, data: T? = null): JsonResponse<T> {
             return JsonResponse(code, message, data)
         }
     }
