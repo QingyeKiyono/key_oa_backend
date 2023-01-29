74b367223fcabdb6cd804b3fce7eda38e42ddb91
jyjdal <1604070358@qq.com>
2023-01-29 18:32:43 +0800

    Remove unused dependency and reformat build script.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,32 +13,15 @@ plugins {
 }
 
 repositories {
-    maven {
-        url = uri("https://maven.aliyun.com/repository/public/")
-    }
-    maven {
-        url = uri("https://maven.aliyun.com/repository/central")
-    }
-    maven {
-        url = uri("https://maven.aliyun.com/repository/google")
-    }
-    maven {
-        url = uri("https://maven.aliyun.com/repository/gradle-plugin")
-    }
-    maven {
-        url = uri("https://maven.aliyun.com/repository/spring")
-    }
-    maven {
-        url = uri("https://maven.aliyun.com/repository/spring-plugin")
-    }
-    maven {
-        url = uri("https://maven.aliyun.com/repository/grails-core")
-    }
-    maven {
-        url = uri("https://maven.aliyun.com/repository/apache-snapshots")
-    }
+    maven { url = uri("https://maven.aliyun.com/repository/public/") }
+    maven { url = uri("https://maven.aliyun.com/repository/central") }
+    maven { url = uri("https://maven.aliyun.com/repository/google") }
+    maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+    maven { url = uri("https://maven.aliyun.com/repository/spring") }
+    maven { url = uri("https://maven.aliyun.com/repository/spring-plugin") }
+    maven { url = uri("https://maven.aliyun.com/repository/grails-core") }
+    maven { url = uri("https://maven.aliyun.com/repository/apache-snapshots") }
     mavenLocal()
-    mavenCentral()
 }
 
 allOpen {
@@ -48,8 +31,7 @@ allOpen {
 }
 
 dependencies {
-    // Spring projects
-    // Starters
+    // Spring Boot starters and tools
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-aop")
@@ -57,9 +39,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    // Starters - test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    // Other spring dependencies
     implementation("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     runtimeOnly("org.springframework.boot:spring-boot-devtools")
@@ -70,22 +50,15 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     // Development tools
-    // Lombok
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
-    testCompileOnly("org.projectlombok:lombok")
-    testAnnotationProcessor("org.projectlombok:lombok")
-    // Guava and Apache commons
-    implementation("com.google.guava:guava:31.1-jre")
     implementation("org.apache.commons:commons-lang3:3.12.0")
-    implementation("org.apache.commons:commons-pool2:2.11.1")
+
     // Jwt dependency
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
-
-    // Drivers and Data access
-    runtimeOnly("mysql:mysql-connector-java:8.0.31")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+
+    // Drivers
+    runtimeOnly("mysql:mysql-connector-java:8.0.31")
 }
 
 group = "com.key"
