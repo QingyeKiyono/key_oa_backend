1c59814079bc5ddb6bdf26e46de61d6c00cc0380
jyjdal <1604070358@qq.com>
2023-02-15 16:06:01 +0800

    Refactor CustomControllerAdvice's code.

--- src/main/kotlin/oa/config/CustomControllerAdvice.kt
+++ src/main/kotlin/oa/config/CustomControllerAdvice.kt
@@ -8,25 +8,28 @@ import oa.common.logger
 import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.MalformedJwtException
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
+@ResponseBody
 @RestControllerAdvice
 class CustomControllerAdvice {
     val log by logger()
 
+    @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler(UsernameNotFoundException::class)
-    fun usernameNotFound(exception: UsernameNotFoundException): ResponseEntity<JsonResponse<Unit>> {
-        val response: JsonResponse<Unit> = error("A0201", "用户账户不存在")
-        return ResponseEntity.ok().body(response)
+    fun usernameNotFound(exception: UsernameNotFoundException): JsonResponse<Unit> {
+        return error("A0201", "用户账户不存在")
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler(BadCredentialsException::class)
-    fun badCredentialsExceptionHandler(exception: BadCredentialsException): ResponseEntity<JsonResponse<Unit>> {
-        val response: JsonResponse<Unit> = when (exception.message) {
+    fun badCredentialsExceptionHandler(exception: BadCredentialsException): JsonResponse<Unit> {
+        return when (exception.message) {
             PASSWORD_WRONG -> error("A0210", "用户密码错误")
             TOKEN_NOT_FOUND -> error("A0301", "访问未授权")
             else -> {
@@ -36,34 +39,33 @@ class CustomControllerAdvice {
                 error("A0300", "访问权限异常")
             }
         }
-        return ResponseEntity.ok().body(response)
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler(MalformedJwtException::class)
-    fun malformedJwtExceptionHandler(exception: MalformedJwtException): ResponseEntity<JsonResponse<Unit>> {
-        val response: JsonResponse<Unit> = error("A0403", "Token非法")
-        return ResponseEntity.ok().body(response)
+    fun malformedJwtExceptionHandler(exception: MalformedJwtException): JsonResponse<Unit> {
+        return error("A0403", "Token非法")
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler(ExpiredJwtException::class)
-    fun expiredJwtExceptionHandler(exception: ExpiredJwtException): ResponseEntity<JsonResponse<Unit>> {
-        val response: JsonResponse<Unit> = error("A0404", "Token已过期")
-        return ResponseEntity.ok().body(response)
+    fun expiredJwtExceptionHandler(exception: ExpiredJwtException): JsonResponse<Unit> {
+        return error("A0404", "Token已过期")
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler(IllegalArgumentException::class)
-    fun illegalArgumentExceptionHandler(exception: IllegalArgumentException): ResponseEntity<JsonResponse<Unit>> {
+    fun illegalArgumentExceptionHandler(exception: IllegalArgumentException): JsonResponse<Unit> {
         if (log.isInfoEnabled) {
             log.info("Request argument is illegal: {}.", exception.message, exception)
         }
-        val response: JsonResponse<Unit> = error("A0402", "无效的用户输入")
-        return ResponseEntity.ok().body(response)
+        return error("A0402", "无效的用户输入")
     }
 
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception::class)
-    fun globalExceptionHandler(exception: Exception): ResponseEntity<JsonResponse<String>> {
+    fun globalExceptionHandler(exception: Exception): JsonResponse<String> {
         log.error("Exception occurred:\n ", exception)
-        val response: JsonResponse<String> = error("B0001", "", exception.message ?: "")
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response)
+        return error("B0001", "", exception.message ?: "")
     }
 }
