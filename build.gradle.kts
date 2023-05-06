05206ae728f7074b1aa5f3e3d20b013aeb35192b
jyjdal <1604070358@qq.com>
2023-05-06 16:46:25 +0800

    Update Spring Boot's version to 3.0.6.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("plugin.jpa") version "1.7.22"
     kotlin("plugin.allopen") version "1.7.22"
 
-    id("org.springframework.boot") version "3.0.2"
+    id("org.springframework.boot") version "3.0.6"
 }
 
 apply(plugin = "io.spring.dependency-management")
